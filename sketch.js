e8cdb7fb4b6416caad822e6fc5da740dc17553f7
Mommocmoc <mommocmoc@gmail.com>
2018-03-14 22:31:06 +0900

    time bubble

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+var s ;
+var shapeColor ;
+var preS = 0 ;
+var curS ;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	shapeColor = color(200,0,0);
 }
 
 function draw() {
+	clear();
+	//background(0,126,255);
+	s=50+second()*10;
+  curS = millis();
+	noStroke();
+  fill(shapeColor)
+	ellipse(windowWidth/2,windowHeight/2,10+s,10+s);
+  if(curS-preS > 1000){
+    preS = curS ;
+    colorChange();
+  }
+}
 
+function colorChange(){
+  shapeColor = color(random(255),random(255),random(255))
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
