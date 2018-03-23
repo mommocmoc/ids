7f9ee9884083aaa8024195fea348e12cc7385f6f
mommocmoc <mommocmoc@gmail.com>
2018-03-23 15:26:02 +0900

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -9,12 +9,12 @@ function setup() {
 
 function draw() {
 	clear();
-	//background(0,126,255);
+	background(shapeColor);
 	s=50+second()*10;
   curS = millis();
 	noStroke();
   fill(shapeColor)
-	ellipse(windowWidth/2,windowHeight/2,15+s,15+s);
+	//ellipse(windowWidth/2,windowHeight/2,15+s,15+s);
   if(curS-preS > 1000){
     preS = curS ;
     colorChange();
